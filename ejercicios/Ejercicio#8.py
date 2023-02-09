d3b19b8dec89c3b9c0fdde05f295a26aa6f9122d
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-02-09 16:00:50 -0500

    Initial commit

---
+++ ejercicios/Ejercicio#8.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    def inversa(a):
+
+        mayor = list(reversed(a))
+        return(mayor)
+    suma = [1, 2, 3, 4, 5, 6]
+    print("Tu lista de mayor a menor quedaria asi:\n", inversa(suma))
