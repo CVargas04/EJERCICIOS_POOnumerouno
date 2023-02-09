0c65133c37e7ed646b19c04de80872e83f274059
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-02-09 16:49:29 -0500

    Initial commit

---
+++ ejercicios/Ejercicio#14.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    def mediaritmetica(a):
+        total=sum(a)/len(a)
+        return(total)
+
+    numero=[1,2,3,4,5,6,7]
+    print("La media aritmetica es: ", mediaritmetica(numero))
