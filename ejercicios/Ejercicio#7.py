b4c2b5682c6245d7f018a02cc29b8cbff795e769
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-02-09 15:56:14 -0500

    Initial commit

---
+++ ejercicios/Ejercicio#7.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    #Numero mas grande
+    maximo = [3, 4, 6, 9, 2]
+
+    print("La mayor es:", max(maximo))
+    print(" ")
+    #Numero mas pequeño
+    minimo=[3,7,4,9,1]
+    print("La menor es:", min(minimo))
