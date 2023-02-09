e154311f7b01bec9ca55a696076f4d1e00dcb890
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-02-09 16:42:53 -0500

    Initial commit

---
+++ ejercicios/Ejercicio#13.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    numero1=int(input("Digite numero\n"))
+    numero2=int(input("Digite segundo numero\n"))
+    suma=numero1+numero2
+    resta=numero1-numero2
+    multipliacion=numero1*numero2
+    division=numero1/numero2
+print("La suma de ",numero1,"y", numero2, "es:", suma)
+print("La resta de ",numero1,"y", numero2, "es:", resta)
+print("La multiplicacion de ",numero1,"y", numero2, "es:", multipliacion)
+print("La division de ",numero1,"y", numero2, "es:", division)
